ef24507f134336d456dd05b22ab5d70e06cfd724
Lewis Maitland <lewisgcm@gmail.com>
2018-10-20 19:21:47 +0100

    adding simplicst auto-reload

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000;
 
-app.get('/', (req: any, res: any) => res.send( "Hello World" ) )
+app.use( express.static('public') );
 
 app.listen( port, () => console.log(`Example app listening on port ${port}!`) );
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,10 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-    entry: {
-        ui: "./src/ui/index.tsx",
-        server: "./src/server.ts"
-    },
-    output: {
-        ui: {
-            path: __dirname + "/build/public",
-            filename: "ui-bundle.js"
-        },
-        server: {
-            path: __dirname + "/build",
-            filename: "server.js"
-        }
-    },
+var base_config = {
     devtool: "source-map",
     resolve: {
         extensions: [ ".ts", ".tsx", ".js", ".json", ".html" ]
     },
-    target: 'node',
-    /*node: {
-        console: true,
-        net: 'empty',
-        tls: 'empty',
-        dns: 'empty',
-        fs:  'empty'
-    },*/
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
@@ -35,10 +13,32 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
+    }
+};
+
+var ui = Object.assign({}, base_config, {
+    entry: './src/ui/index.tsx',
+    output: {
+        path: __dirname + "/build/public",
+        filename: "ui-bundle.js"
     },
+    target: 'web',
     plugins: [
         new CopyWebpackPlugin([
             { from: 'ui/index.html', context: 'src' }
         ])
     ]
-};
+});
+
+var server = Object.assign({}, base_config, {
+    entry: './src/server.ts',
+    output: {
+        path: __dirname + "/build",
+        filename: "server.js"
+    },
+    target: 'node'
+});
+
+module.exports = [
+    ui, server
+];
